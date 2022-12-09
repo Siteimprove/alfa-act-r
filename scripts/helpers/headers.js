b71dff3ef9c2a853f2e09d42d023ccaf0811c1e5
Jean-Yves Moyen <jym@siteimprove.com>
2022-12-09 14:49:50 +0100

    Remove generated file

--- scripts/helpers/headers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.filter = void 0;
-const whitelist = require("./headers.json");
-function filter(headers) {
-    return headers.filter((header) => whitelist.includes(header.name));
-}
-exports.filter = filter;
-//# sourceMappingURL=headers.js.map
